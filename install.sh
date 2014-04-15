0bbbded261ac1421f36f31b7f61b8c9cb7cbbb80
Stefan Berggren <nsg@nsg.cc>
2014-04-15 12:25:08 +0200

    checkpoint

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+message() {
+	echo
+	echo "$1"
+	echo
+}
+
+message "Install packages needed by ansible"
 sudo apt-get install python-pip
 sudo pip install ansible
+
+message "Run ansible playbook"
 export ANSIBLE_NOCOWS=1 # kill the cow!
-ansible-playbook -c local -i localhost_inventory site.yml
+ansible-playbook -c local -s -K -i localhost_inventory site.yml
