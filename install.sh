015ceacb5f8fc730e0d875a86ead31893860f77f
Stefan Berggren <nsg@nsg.cc>
2017-04-01 14:47:21 +0200

    Use a virtualenv, pin version and better logic.

--- install.sh
+++ install.sh
@@ -4,16 +4,42 @@ message() {
 	echo "$1"
 }
 
-if ! type pip; then
-	message "Install python-pip and dev"
-	sudo apt-get install python-pip python-dev libssl-dev
+get_my_path() {
+	pushd $(dirname $0) > /dev/null
+	pwd -P
+	popd > /dev/null
+}
+
+install_package() {
+	if ! dpkg -s $1 2>&1 > /dev/null; then
+		sudo apt install $1
+	fi
+}
+
+SCRIPTPATH="$(get_my_path)"
+
+# Setup a virtualenv with Ansible, install it if needed and
+# activate the environment.
+if ! type virtualenv 1> /dev/null 2>&1; then
+	message "virtualenv is missing, I will install it"
+	sudo apt install python-virtualenv
 fi
 
-if ! type ansible; then
+if [ ! -e "$SCRIPTPATH/.env" ]; then
+	virtualenv "$SCRIPTPATH/.env"
+fi
+
+. $SCRIPTPATH/.env/bin/activate
+
+# Ansible needs a few libs and gcc, so make sure that they are installed
+install_package python-dev
+install_package libssl-dev
+install_package gcc
+
+if [ ! -e "$SCRIPTPATH/.env/bin/ansible" ]; then
 	message "Install ansible with pip"
-	sudo pip install ansible
+	pip install ansible==2.2.2
 fi
 
-message "Run ansible playbook"
 export ANSIBLE_NOCOWS=1 # kill the cow!
 ansible-playbook -c local -s -K -i 127.0.0.1, --diff site.yml
