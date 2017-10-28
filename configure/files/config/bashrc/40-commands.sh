dea7c93188ef95cf1f02c3890721a3a49403c535
Stefan Berggren <nsg@nsg.cc>
2017-10-28 14:37:48 +0200

    c: added support for multiple matches.

--- configure/files/config/bashrc/40-commands.sh
+++ configure/files/config/bashrc/40-commands.sh
@@ -16,8 +16,20 @@ kinit() {
 c() {
     local scope=$1
     local project=$2
+    local number=$3
 
-    cd ~/code/*${scope}*/*${project}*
+    local projects=(~/code/*${scope}*/*${project}*)
+    if [ ${#projects[@]} -gt 1 ]; then
+        if [ ! -z $number ] && [ $number -lt ${#projects[@]} ]; then
+            cd ${projects[$number]}
+        else
+            for (( n=0; n<${#projects[@]}; n++ )); do
+                echo "$n) ${projects[$n]}"
+            done
+        fi
+    else
+        cd ${projects[0]}
+    fi
 }
 
 # Workaround for https://github.com/neovim/neovim/issues/6802
