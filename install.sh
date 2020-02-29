32fd72cffd77d206a1569d937e5903bf05c8f1b3
Stefan Berggren <nsg@nsg.cc>
2020-02-29 10:24:27 +0100

    Update for 20.04

--- install.sh
+++ install.sh
@@ -35,7 +35,7 @@ fi
 
 if [[ $(release) == "18.04" ]]; then
 	:
-elif [[ $(release) == "19.10" ]]; then
+elif [[ $(release) == "20.04" ]]; then
 	:
 else
 	die "This is not tested on $(release)"
@@ -45,24 +45,24 @@ fi
 # activate the environment.
 if ! type virtualenv 1> /dev/null 2>&1; then
 	message "virtualenv is missing, I will install it"
-	sudo apt install python-virtualenv
+	sudo apt install python3-virtualenv
 fi
 
 if [ ! -e "$SCRIPTPATH/.env" ]; then
-	virtualenv "$SCRIPTPATH/.env"
+	virtualenv -p python3 "$SCRIPTPATH/.env"
 fi
 
 . $SCRIPTPATH/.env/bin/activate
 
 # Ansible needs a few libs and gcc, so make sure that they are installed
-install_package python-dev
+install_package python3-dev
 install_package libssl-dev
 install_package libffi-dev
 install_package gcc
 
 if [ ! -e "$SCRIPTPATH/.env/bin/ansible" ]; then
 	message "Install ansible with pip"
-	pip install ansible==2.8.6
+	pip install ansible==2.9.5
 fi
 
 export ANSIBLE_NOCOWS=1 # kill the cow!
