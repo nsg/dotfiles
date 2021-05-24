4a18188e6f8d99a980ea0dcf5e04b6e1ef8cb225
Stefan Berggren <stefan.berggren@svt.se>
2021-05-24 17:24:23 +0200

    Build KUBECONFIG

--- configure/files/config/bashrc/01-env.sh
+++ configure/files/config/bashrc/01-env.sh
@@ -6,3 +6,13 @@ export EDITOR=vim
 export ANSIBLE_A_USER=stbe02
 export PATH="~/bin:$PATH"
 export GIT_CEILING_DIRECTORIES=/afs/stacken.kth.se
+
+# Build KUBECONFIG
+KUBECONFIG=""
+for k in $HOME/.kube/*; do
+    if [[ $k == *.yaml ]] || [[ $k == *.yml ]]; then
+        KUBECONFIG="$k:$KUBECONFIG"
+    fi
+done
+KUBECONFIG="${KUBECONFIG:0:-1}"
+export KUBECONFIG
